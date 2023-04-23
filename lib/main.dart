4195f4b5f0ec3e7acba4f89ef8a5dfc801dcf2aa
tyange <usun16@gmail.com>
2023-04-23 18:21:42 +0900

    linked providers with MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:favorite_places/screens/places.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:favorite_places/screens/places.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
@@ -28,7 +30,9 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-    const MyApp(),
+    const ProviderScope(
+      child: MyApp(),
+    ),
   );
 }
 
